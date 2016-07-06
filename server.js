7060f6222c17b18eabe2f3158fffb2835040d1a8
Udai <udaiyamnm@gmail.com>
2016-07-06 12:29:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
